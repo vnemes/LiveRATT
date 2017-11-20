d360e715fd8b882524f47487c8583c3d9f6e25b6
vendettavn <vladnemes5@gmail.com>
2017-11-20 09:46:34 +0200

    Refactored Route Component binding the route by direction

--- client/LiveRATT/src/app/route/route.component.ts
+++ client/LiveRATT/src/app/route/route.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {RouteContainer} from "./routecontainer";
+import {Route} from "./route";
 
 @Component({
   selector: 'app-route',
@@ -9,7 +10,14 @@ import {RouteContainer} from "./routecontainer";
 export class RouteComponent {
   @Input() private selectedRouteChild:RouteContainer;
   @Input() private direction:String;
+
   constructor() {
   }
+
+  getRouteByDirection():Route{
+    console.log(this.selectedRouteChild);
+    return this.direction == "inbound" ? this.selectedRouteChild.inbound : this.selectedRouteChild.outbound;
+  }
+
 
 }
